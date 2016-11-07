904ad4725201b24ba4ba77b205a8c6ed1fd565a8
Jorge Vargas Silva <jv.vargass@gmail.com>
2016-11-07 01:15:37 -0300

    Validates names

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,8 @@ class Category < ApplicationRecord
   has_many :categorizations
   has_many :debts, through: :categorizations
 
+  validates :name, presence: true
+
   def to_s
     name
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :debts
 
+  validates :name, presence: true
+
   def to_s
     name
   end
